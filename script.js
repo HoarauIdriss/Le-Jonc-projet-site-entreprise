fb0a354b495a7e1b607c2b8a333f4e58a8892a4c
Hoarau Idriss <39042806+HoarauIdriss@users.noreply.github.com>
2018-12-13 12:34:00 +0400

    new

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
+/* form ----------------------------------------------------------------------------*/
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+(function () {
+  'use strict';
+  window.addEventListener('load', function () {
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    var forms = document.getElementsByClassName('needs-validation');
+    // Loop over them and prevent submission
+    var validation = Array.prototype.filter.call(forms, function (form) {
+      form.addEventListener('submit', function (event) {
+        if (form.checkValidity() === false) {
+          event.preventDefault();
+          event.stopPropagation();
+        }
+        form.classList.add('was-validated');
+      }, false);
+    });
+  }, false);
+})()
+
+
 /*----Javascript----*/
 /*-Evenement click-*/
 
@@ -9,12 +30,9 @@ var element = document.getElementById('clickme');
  
   element.removeEventListener('click', function() {
     alert("Votre commercial vous attend au 06 92 00 32 10, Merci de votre visite");
-  });
+  })
 
   
- 
-
-
 
 
 /*-jquery---------------------------------------------------------------------------*/
@@ -30,7 +48,7 @@ $(function(){
   $("#monBouton2") .click(function(){
       $("html, body").animate({scrollTop: 0},"slow");
   });
-});
+})
 
 /*-Modal-*/
 
@@ -55,25 +73,7 @@ $('#myModal3').modal(options);
 
 
 
-/* form ----------------------------------------------------------------------------*/
-// Example starter JavaScript for disabling form submissions if there are invalid fields
-(function () {
-  'use strict';
-  window.addEventListener('load', function () {
-    // Fetch all the forms we want to apply custom Bootstrap validation styles to
-    var forms = document.getElementsByClassName('needs-validation');
-    // Loop over them and prevent submission
-    var validation = Array.prototype.filter.call(forms, function (form) {
-      form.addEventListener('submit', function (event) {
-        if (form.checkValidity() === false) {
-          event.preventDefault();
-          event.stopPropagation();
-        }
-        form.classList.add('was-validated');
-      }, false);
-    });
-  }, false);
-})();
+
 
 
 
